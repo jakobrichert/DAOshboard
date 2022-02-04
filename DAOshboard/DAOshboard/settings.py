792efb6d362d34ac2e75edfebd734d990661c58c
Jakob <jakobquantum@tuta.io>
2022-02-04 09:30:40 -0700

    Update settings.py

--- DAOshboard/DAOshboard/settings.py
+++ DAOshboard/DAOshboard/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!b!4rfoc9b39+=$%wo0pcw8^@t=(x9v@p!%4ag&%d!lla$xq9y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
